bf9f6176dc15d3f3098cf2897d40a3d12d6a601b
David  Paul Kamy <127941418+kamCodeZ@users.noreply.github.com>
2024-04-16 14:33:31 +0000

    Your commit message

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,5 @@
-// AppContainer.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Loader from './Loader';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation';
 import Calculator from './apps/calculator/Calculator';
@@ -8,24 +8,117 @@ import BirthdayReminder from './apps/birthday-reminder/BirthdayReminder';
 import Lapp from './apps/B/Lapp';
 import Home from './Home';
 
-
-
 const AppContainer = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate data loading with setTimeout
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Adjust the duration to 3 seconds (3000 milliseconds)
+
+    // Clean up timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Router>
-      <div>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/calculator" element={<Calculator/>} />
-          <Route path="/quote-generator" element={<QuoteBox/>} />
-          <Route path="/birthday-reminder" element={<BirthdayReminder/>} />
-          <Route path='/B' element={ <Lapp/>} />
-          {/* Add routes for other components */}
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Router>
+          <div>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/calculator" element={<Calculator />} />
+              <Route path="/quote-generator" element={<QuoteBox />} />
+              <Route path="/birthday-reminder" element={<BirthdayReminder />} />
+              <Route path='/B' element={<Lapp />} />
+              {/* Add routes for other components */}
+            </Routes>
+          </div>
+        </Router>
+      )}
+    </div>
   );
 };
 
 export default AppContainer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // AppContainer.js
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Navigation from './Navigation';
+// import Calculator from './apps/calculator/Calculator';
+// import QuoteBox from './apps/quote-generator/QuoteBox';
+// import BirthdayReminder from './apps/birthday-reminder/BirthdayReminder';
+// import Lapp from './apps/B/Lapp';
+// import Home from './Home';
+
+
+
+// const AppContainer = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Navigation />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/calculator" element={<Calculator/>} />
+//           <Route path="/quote-generator" element={<QuoteBox/>} />
+//           <Route path="/birthday-reminder" element={<BirthdayReminder/>} />
+//           <Route path='/B' element={ <Lapp/>} />
+//           {/* Add routes for other components */}
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default AppContainer;
